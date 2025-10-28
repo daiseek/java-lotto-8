654cdc3db051aa36eabcd847c7306b1abbb20871
daiseek <dnjsgml0578@gmail.com>
2025-10-28 23:16:04 +0900

    feat : validatePositiveInteger() 메서드 정의 - 양의 정수인지 판단하는 유효성 메서드 정의

--- src/main/java/lotto/validate/InputValidate.java
+++ src/main/java/lotto/validate/InputValidate.java
@@ -2,5 +2,11 @@ package lotto.validate;
 
 public class InputValidate {
     // TODO : 유효성 검증 메서드 구현
+    
     //  양의 정수 외의 숫자를 입력하면 에러를 발생시키는 메서드
+    public static void validatePositiveInteger(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("[ERROR] 양의 정수를 입력해야 합니다.");
+        }
+    }
 }
