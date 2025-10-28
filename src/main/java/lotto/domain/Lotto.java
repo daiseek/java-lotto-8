2023fba603e78bfb13f2f01bdc933889dec543ca
daiseek <dnjsgml0578@gmail.com>
2025-10-28 22:49:44 +0900

    feat : sortLotto() 메서드 구현 - 로또 번호 리스트를 오름차순 정렬하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -17,12 +19,10 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
-    //  - 로또 번호 리스트 오름차순 정렬 메서드
-    // 구현후 validate로 이동
-    //  - 로또 번호 리스트 중복 검사 메서드
-    //  - 로또 번호 리스트 범위 검사 메서드
-    //  - 로또 번호 리스트 범위 미만 검사 메서드
-    //  - 로또 번호 리스트 범위 초과 검사 메서드
 
-    // - 당첨 번호 비교 메서드 
+    // 로또 번호 리스트 오름차순 정렬 메서드
+    public void sortLotto() {
+        Collections.sort(numbers);
+    }
+    
 }
