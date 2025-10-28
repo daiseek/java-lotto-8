98e8034f33ee75bc0944d915f47fb1fbc65ba71b
daiseek <dnjsgml0578@gmail.com>
2025-10-28 23:21:35 +0900

    feat : inputLottoPurchaseAmount() 메서드 정의 - 사용자에게 로또 구입 금액을 입력받는 메서드 - 유효성 검증 메서드 사용

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,8 +1,22 @@
 package lotto.view;
 
+import static lotto.validate.InputValidate.validatePositiveInteger;
+import static lotto.validate.InputValidate.validateInteger;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     // TODO : 입력 메서드 구현
-    //  - 로또 구입 금액을 입력받는 메서드
+
+    // 로또 구입 금액을 입력받는 메서드
+    public int inputLottoPurchaseAmount() {
+        String input = Console.readLine();
+        int PurchaseAmount = validateInteger(input);
+        validatePositiveInteger(PurchaseAmount);
+        return PurchaseAmount;
+    }
+
+
     //  - 당첨 번호를 쉼표 기준으로 한 줄로 입력받는 메서드
     //  - 보너스 번호 입력 메서드
 }
