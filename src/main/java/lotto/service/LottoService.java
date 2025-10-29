7e39eb9ed01df6e75bb61b2b1cbde423c348898f
daiseek <dnjsgml0578@gmail.com>
2025-10-29 21:51:46 +0900

    feat : generateLotto() 메서드 정의 & 상수 위치 이동 - 로또 번호 생성 기능 구현 - 해당 기능에서 사용되는 상수를 Service 계층에서 InputUtil로 이동

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -4,9 +4,6 @@ public class LottoService {
     // TODO : Lotto 관련 로직을 모아서 비즈니스 로직 구현 
 
     private static final int LOTTO_PRICE = 1000;
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
-    private static final int LOTTO_SIZE = 6;
 
     // 로또 구매 후 로또 개수 반환 메서드 
     public int purchaseLotto(int purchaseAmount) {
@@ -14,6 +11,7 @@ public class LottoService {
     }
 
     // 로또 당첨 확인 메서드
+
 
     // 수익률 계산 메서드
 
--- src/main/java/lotto/util/InputUtil.java
+++ src/main/java/lotto/util/InputUtil.java
@@ -1,13 +1,25 @@
 package lotto.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class InputUtil {
     // TODO : Input 관련 유틸리티 메서드 정의
-    //  구입금 만큼 로또 개수로 환산하는 메서드
+
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+
     //  로또 번호 생성 메서드 - 로또 범위의 끝 값을 상수로 할당후 사용
+    public List<Integer> generateLotto() {
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_SIZE);
+    }
+    
+
     //  보너스 번호를 입력받는 메서드
+    
     //  보너스 번호를 로또 번호 리스트 끝에 할당해주는 메서드
+    
     //  로또 번호를 쉼표 기준으로 구분하는 메서드
-
-
 
 }
