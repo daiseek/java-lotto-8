97626bfdde7f44f4d65337ebe584c9f22fef767c
daiseek <dnjsgml0578@gmail.com>
2025-10-29 22:00:01 +0900

    feat : 쉼표를 기준으로 파싱할 수 없을 때 예외 처리 기능 구현

--- src/main/java/lotto/validate/InputValidate.java
+++ src/main/java/lotto/validate/InputValidate.java
@@ -10,11 +10,19 @@ public class InputValidate {
         }
     }
 
+    //  정수 외의 숫자를 입력하면 에러를 발생시키는 메서드
     public static int validateInteger(String number) {
         try {
             return Integer.parseInt(number);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 양의 정수를 입력해야 합니다.");
+        }
+    }
+
+    // 쉼표를 기준으로 파싱할 수 없을때 예외 처리 메서드
+    public static void validateComma(String input) {
+        if (!input.contains(",")) {
+            throw new IllegalArgumentException("[ERROR] 쉼표를 기준으로 파싱할 수 없습니다.");
         }
     }
 }
