f4f2b635fe450d8a6d60912f1e457b60b0dc9e9c
daiseek <dnjsgml0578@gmail.com>
2025-10-29 22:07:08 +0900

    feat : 당첨 번호를 쉼표와 함께 입력받는 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,7 +1,11 @@
 package lotto.view;
 
+import static lotto.validate.InputValidate.validateComma;
 import static lotto.validate.InputValidate.validatePositiveInteger;
 import static lotto.validate.InputValidate.validateInteger;
+import static lotto.util.InputUtil.parseLottoNumbers;
+
+import java.util.List;
 
 import camp.nextstep.edu.missionutils.Console;
 
@@ -16,7 +20,11 @@ public class InputView {
         return PurchaseAmount;
     }
 
-
-    //  - 당첨 번호를 쉼표 기준으로 한 줄로 입력받는 메서드
+    //  - 당첨 번호를 쉼표와 함께 한 줄로 입력받는 메서드
+    public List<Integer> inputWinningLottoNumbers() {
+        String input = Console.readLine();
+        validateComma(input);
+        return parseLottoNumbers(input);
+    }
     //  - 보너스 번호 입력 메서드
 }
