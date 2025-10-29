3b18e7b31e3dac03e4b0619e169d1f3a52aa593b
daiseek <dnjsgml0578@gmail.com>
2025-10-29 21:39:31 +0900

    feat :LottoDto 클래스 구현

--- src/main/java/lotto/dto/LottoDto.java
+++ src/main/java/lotto/dto/LottoDto.java
@@ -1,4 +1,35 @@
 package lotto.dto;
 
+import java.util.List;
+
+// 로또 관련 입력 데이터를 저장하는 클래스
 public class LottoDto {
+
+    private int purchaseAmount;
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+
+    public LottoDto(int purchaseAmount, List<Integer> winningNumbers, int bonusNumber) {
+        this.purchaseAmount = purchaseAmount;
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getPurchaseAmount() {
+        return purchaseAmount;
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    public static LottoDto of(int purchaseAmount, List<Integer> winningNumbers, int bonusNumber) {
+        return new LottoDto(purchaseAmount, winningNumbers, bonusNumber);
+    }
+
 }
+
