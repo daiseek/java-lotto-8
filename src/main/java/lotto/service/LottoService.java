59c767c39ccf44d7786b351a18ff7551535b1adb
daiseek <dnjsgml0578@gmail.com>
2025-10-29 22:44:42 +0900

    fix: purchaseLotto() 메서드가 구매한 로또 개수를 반환하도록 수정 & 로또 개수 만큼 생성하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,17 +1,39 @@
 package lotto.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lotto.domain.Lotto;
+import lotto.util.InputUtil;
+
 public class LottoService {
     // TODO : Lotto 관련 로직을 모아서 비즈니스 로직 구현 
 
     private static final int LOTTO_PRICE = 1000;
+    private int lottoCount;
 
     // 로또 구매 후 로또 개수 반환 메서드 
     public int purchaseLotto(int purchaseAmount) {
-        return purchaseAmount / LOTTO_PRICE;
+        lottoCount = purchaseAmount / LOTTO_PRICE;
+        return lottoCount;
     }
 
-    // 로또 당첨 확인 메서드
+    // 로또 생성 메서드
+    public List<Lotto> generateLottos() {
+        List<Lotto> lottos = new ArrayList<>();
+        
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> lottoNumbers = InputUtil.generateLotto();
+            Lotto lotto = Lotto.of(lottoNumbers);
+            lottos.add(lotto);
+    } 
+        return lottos;
+    }
 
+    // 로또 당첨 확인
+
+
+    //  보너스 번호와 로또 번호를 비교하는 메서드
 
     // 수익률 계산 메서드
 
