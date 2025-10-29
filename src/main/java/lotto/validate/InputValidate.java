9fcb44608d89cc1f0e34677d2f0dbd0e1e7a3f25
daiseek <dnjsgml0578@gmail.com>
2025-10-29 22:11:42 +0900

    feat : 로또 번호 범위인지 검증하는 기능 구현

--- src/main/java/lotto/validate/InputValidate.java
+++ src/main/java/lotto/validate/InputValidate.java
@@ -19,6 +19,13 @@ public class InputValidate {
         }
     }
 
+    //  로또 번호 범위 검사 메서드
+    public static void validateRangeLottoNumber(int number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
     // 쉼표를 기준으로 파싱할 수 없을때 예외 처리 메서드
     public static void validateComma(String input) {
         if (!input.contains(",")) {
