b6eb57bf04761da2ec3871ccf3db42b4cbc3e7e5
daiseek <dnjsgml0578@gmail.com>
2025-10-29 22:06:39 +0900

    feat : 로또 번호를 쉼표 기준으로 파싱하는 기능 구현 - 각 메서드에 static 예약어 추가

--- src/main/java/lotto/util/InputUtil.java
+++ src/main/java/lotto/util/InputUtil.java
@@ -1,7 +1,10 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class InputUtil {
     // TODO : Input 관련 유틸리티 메서드 정의
@@ -11,15 +14,17 @@ public class InputUtil {
     private static final int LOTTO_SIZE = 6;
 
     //  로또 번호 생성 메서드 - 로또 범위의 끝 값을 상수로 할당후 사용
-    public List<Integer> generateLotto() {
+    public static List<Integer> generateLotto() {
         return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_SIZE);
     }
     
-
-    //  보너스 번호를 입력받는 메서드
+    //  보너스 번호와 로또 번호를 비교하는 메서드
     
-    //  보너스 번호를 로또 번호 리스트 끝에 할당해주는 메서드
-    
-    //  로또 번호를 쉼표 기준으로 구분하는 메서드
+    //  로또 번호를 쉼표 기준으로 구분(파싱)하는 메서드
+    public static List<Integer> parseLottoNumbers(String input) {
+        return Arrays.stream(input.split(","))
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+    }
 
 }
