6e7c3b3d3b4d95fe99c07a91955744dc4b4e63dc
daiseek <dnjsgml0578@gmail.com>
2025-10-30 23:43:22 +0900

    fix: 각 예외 상황별로 Error Message 정의

--- src/main/java/lotto/error/ErrorMessage.java
+++ src/main/java/lotto/error/ErrorMessage.java
@@ -3,9 +3,13 @@ package lotto.error;
 public class ErrorMessage {
     // TODO : 에러 메시지 정의
     //  이때 "[ERROR]" 메시지를 포함하여 출력
-    //  - 로또 번호 개수 초과 에러 메시지
+
+    //  - 양의 정수 외의 숫자 입력시 에러 메시지
+    public static final String INVALID_POSITIVE_INTEGER_MESSAGE = "[ERROR] 양의 정수를 입력해야 합니다.";
     //  - 로또 번호 중복 에러 메시지
+    public static final String INVALID_DUPLICATE_LOTTO_MESSAGE = "[ERROR] 로또 번호는 중복되지 않아야 합니다.";
     //  - 로또 번호 범위 초과 에러 메시지
-    //  - 로또 번호 범위 미만 에러 메시지
-    //  - 로또 번호 범위 초과 에러 메시지
+    public static final String INVALID_OVER_LOTTO_NUMBER_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    // 쉼표 기준 파싱할때 에러 메시지
+    public static final String INVALID_COMMA_MESSAGE = "[ERROR] 쉼표를 기준으로 파싱할 수 없습니다.";
 }
