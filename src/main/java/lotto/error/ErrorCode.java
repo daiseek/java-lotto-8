b506a5dd59db48bdc0d52c53bb0351e3ae35634c
daiseek <dnjsgml0578@gmail.com>
2025-10-30 23:39:40 +0900

    feat : 예외 상황별로 Error Code 정의

--- src/main/java/lotto/error/ErrorCode.java
+++ src/main/java/lotto/error/ErrorCode.java
@@ -2,9 +2,14 @@ package lotto.error;
 
 public class ErrorCode {
     // TODO : 에러 코드 정의
-    //  - 로또 번호 개수 초과 에러 코드
+    //  - 양의 정수 외의 숫자 입력시 에러 코드
+    public static final String INVALID_INPUT_INTEGER = "INVALID_INPUT_INTEGER";
     //  - 로또 번호 중복 에러 코드
+    public static final String INVALID_DUPLICATE_LOTTO = "INVALID_DUPLICATE_LOTTO";
     //  - 로또 번호 범위 초과 에러 코드
-    //  - 로또 번호 범위 미만 에러 코드
-    //  - 로또 번호 범위 초과 에러 코드
+    public static final String INVALID_OVER_LOTTO_NUMBER = "INVALID_OVER_LOTTO_NUMBER";
+    // 쉼표 기준 파싱할때 에러 코드
+    public static final String INVALID_COMMA = "INVALID_COMMA";
+
+
 }
