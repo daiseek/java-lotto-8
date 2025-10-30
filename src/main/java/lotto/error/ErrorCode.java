7edfb0c4d75923d75b0b73ce85b836a7ae6a6732
daiseek <dnjsgml0578@gmail.com>
2025-10-30 23:42:59 +0900

    fix: error message와 구분하기 위해 변수명에 CODE 추가

--- src/main/java/lotto/error/ErrorCode.java
+++ src/main/java/lotto/error/ErrorCode.java
@@ -3,13 +3,13 @@ package lotto.error;
 public class ErrorCode {
     // TODO : 에러 코드 정의
     //  - 양의 정수 외의 숫자 입력시 에러 코드
-    public static final String INVALID_INPUT_INTEGER = "INVALID_INPUT_INTEGER";
+    public static final String INVALID_INPUT_INTEGER_CODE = "INVALID_INPUT_INTEGER";
     //  - 로또 번호 중복 에러 코드
-    public static final String INVALID_DUPLICATE_LOTTO = "INVALID_DUPLICATE_LOTTO";
+    public static final String INVALID_DUPLICATE_LOTTO_CODE = "INVALID_DUPLICATE_LOTTO";
     //  - 로또 번호 범위 초과 에러 코드
-    public static final String INVALID_OVER_LOTTO_NUMBER = "INVALID_OVER_LOTTO_NUMBER";
+    public static final String INVALID_OVER_LOTTO_NUMBER_CODE = "INVALID_OVER_LOTTO_NUMBER";
     // 쉼표 기준 파싱할때 에러 코드
-    public static final String INVALID_COMMA = "INVALID_COMMA";
+    public static final String INVALID_COMMA_CODE = "INVALID_COMMA";
 
 
 }
