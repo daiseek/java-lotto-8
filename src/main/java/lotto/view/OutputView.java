fd01e5a62a546f5c7aa81d5fcfdeb9058dd2ab93
daiseek <dnjsgml0578@gmail.com>
2025-10-30 23:20:19 +0900

    feat : 사용자가 구매한 로또 개수를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,6 +4,10 @@ public class OutputView {
 
     // TODO: 출력 메서드 구현
     //  - 사용자가 구매한 갯수 출력 메서드
+    public void printLottos(int lottoCount) {
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
     //  - 당첨 내역 출력 메서드
     //  - 수익률 출력 메서드 + 소수점 둘째 자리에서 반올림
     //  - 에러 메시지 출력 메서드
