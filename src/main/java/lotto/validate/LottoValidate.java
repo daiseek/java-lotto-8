c9ad8405abfe5c4b94b6ca771fc1c26c9a14f03a
daiseek <dnjsgml0578@gmail.com>
2025-10-30 23:50:12 +0900

    fix: Lotto 객체 유효성 검증 클래스에 Error Message 도입

--- src/main/java/lotto/validate/LottoValidate.java
+++ src/main/java/lotto/validate/LottoValidate.java
@@ -3,19 +3,21 @@ package lotto.validate;
 import java.util.HashSet;
 import java.util.List;
 
+import lotto.error.ErrorMessage;
+
 public class LottoValidate {
 
     // 로또 번호 리스트 중복 검사 메서드
     public void validateDuplicateLotto(List<Integer> lottoNumbers) {
         if (lottoNumbers.size() != new HashSet<>(lottoNumbers).size()) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DUPLICATE_LOTTO_MESSAGE);
         }
     }
 
     //  로또 번호 리스트 범위 미만 검사 메서드
     public void validateOverLottoNumber(int lottoNumbers) {
         if (lottoNumbers <= 1 || lottoNumbers >= 45) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.INVALID_OVER_LOTTO_NUMBER_MESSAGE);
         }
     }
 
